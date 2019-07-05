d41ea5a2b503d11ad35c646f2565a93606a31faa
Nuunkung <52436899+nuunkung@users.noreply.github.com>
2019-07-05 12:47:36 +0700

    Add files via upload

---
+++ BoolExample.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(type(True))
+print(type(False))
+
+
+# In[3]:
+
+
+print("Hello"+"World")
+
+
+# In[4]:
+
+
+print("A"*5)
+
+
+# In[8]:
+
+
+print("Welcom to Somsak Shop")
+print("---------------------------------")
+print("Apple x", 3, 30, "THB - ", 3*30, "THB")
+print("Lay x", 3, 10, "THB - ", 3*10, "THB")
+print("Apple 3 30THB 90")
+print("Banana 1 10THB 10")
+print("---------------------------------")
+print("Total ",(3*30)+(3*10), "THB")
+print("Total 100THB")
+print("Thank You : Somsak Shop")
+
+
+# In[10]:
+
+
+print("10"+"5")
+
+
+# In[ ]:
+
+
+
+
